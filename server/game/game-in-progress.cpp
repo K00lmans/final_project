ea48e1f3da5ff4d9087dea3a5ca496158492b4a0
Athena Boose <pestpestthechicken@yahoo.com>
2025-04-21 18:49:57 -0700

    Finished all but main bit of game-in-progress.

--- server/game/game-in-progress.cpp
+++ server/game/game-in-progress.cpp
@@ -2,22 +2,37 @@
 #include <socket-handling/fd-utils.hpp>
 #include "game-in-progress.hpp"
 
+static void delete_bad_player(std::vector<Player> &players, Player &bad_player) {
+    std::swap(bad_player, players[players.size() - 1]);
+    close_except(players[players.size() - 1].fd);
+    players.pop_back();
+}
+
 bool GameInProgress::callback() {
     std::array<epoll_event, 1> event_arr;
     std::span<epoll_event> span(event_arr);
     epoll_event ev = poll.wait(span, -1)[0];
     Player &player_with_event = find_io(ev.data.fd);
     if (ev.events & EPOLLRDHUP || ev.events & EPOLLERR || ev.events & EPOLLHUP) {
-        std::swap(player_with_event, players[players.size() - 1]);
-        close_except(players[players.size() - 1].fd);
+        delete_bad_player(players, player_with_event);
         players.pop_back();
         send_err_msg("Another player unexpectedly closed their connection.");
         return false;
     }
 
-    if (ev.events & EPOLLIN) {}
+    if (ev.events & EPOLLIN) {} // suffering :)
     if (ev.events & EPOLLOUT) {
         SocketStatus result(player_with_event.outbuf.flush(player_with_event.fd));
+        switch (result) {
+        case SocketStatus::Finished:
+        case SocketStatus::Blocked:
+            break;
+        case SocketStatus::Error:
+        case SocketStatus::ZeroReturned:
+            delete_bad_player(players, player_with_event);
+            send_err_msg("Another player unexpectedly closed their connection.");
+            return false;
+        }
     }
 
     return true;
