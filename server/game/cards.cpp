c05d442d5e7ef86b95201c91a8f9008f153e0c68
Athena Boose <pestpestthechicken@yahoo.com>
2025-04-23 21:29:57 -0700

    Restructured files more, broke cards.* into two separate files.

---
+++ server/game/cards.cpp
@@ -0,0 +1,63 @@
+#include "cards.hpp"
+
+
+namespace cards {
+
+const std::array<std::string, 6> suspects{
+    "Colonel Mustard",
+    "Miss Scarlet",
+    "Professor Plum",
+    "Mr. Green",
+    "Mrs. White",
+    "Mrs. Peacock"
+};
+
+const std::array<std::string, 7> weapons{
+    "Rope",
+    "Lead Pipe",
+    "Knife",
+    "Wrench",
+    "Candlestick",
+    "Revolver",
+    "Bottle of Poison",
+};
+
+const std::array<std::string, 9> rooms{
+    "Study",
+    "Hall",
+    "Lounge",
+    "Library",
+    "Billiard Room",
+    "Conservatory",
+    "Ballroom",
+    "Kitchen",
+    "Dining Room",
+};
+
+
+const std::array<std::string, 22> all_cards {
+    "Colonel Mustard",
+    "Miss Scarlet",
+    "Professor Plum",
+    "Mr. Green",
+    "Mrs. White",
+    "Mrs. Peacock",
+    "Rope",
+    "Lead Pipe",
+    "Knife",
+    "Wrench",
+    "Candlestick",
+    "Revolver",
+    "Bottle of Poison",
+    "Study",
+    "Hall",
+    "Lounge",
+    "Library",
+    "Billiard Room",
+    "Conservatory",
+    "Ballroom",
+    "Kitchen",
+    "Dining Room"
+};
+
+}
--- server/game/cards.hpp
+++ server/game/cards.hpp
@@ -4,62 +4,10 @@
 
 namespace cards {
 
-const std::array<std::string, 6> suspects{
-    "Colonel Mustard",
-    "Miss Scarlet",
-    "Professor Plum",
-    "Mr. Green",
-    "Mrs. White",
-    "Mrs. Peacock"
-};
-
-const std::array<std::string, 7> weapons{
-    "Rope",
-    "Lead Pipe",
-    "Knife",
-    "Wrench",
-    "Candlestick",
-    "Revolver",
-    "Bottle of Poison",
-};
-
-const std::array<std::string, 9> rooms{
-    "Study",
-    "Hall",
-    "Lounge",
-    "Library",
-    "Billiard Room",
-    "Conservatory",
-    "Ballroom",
-    "Kitchen",
-    "Dining Room",
-};
-
-const std::array<std::string, 22> all_cards {
-    "Colonel Mustard",
-    "Miss Scarlet",
-    "Professor Plum",
-    "Mr. Green",
-    "Mrs. White",
-    "Mrs. Peacock",
-    "Rope",
-    "Lead Pipe",
-    "Knife",
-    "Wrench",
-    "Candlestick",
-    "Revolver",
-    "Bottle of Poison",
-    "Study",
-    "Hall",
-    "Lounge",
-    "Library",
-    "Billiard Room",
-    "Conservatory",
-    "Ballroom",
-    "Kitchen",
-    "Dining Room"
-};
+extern const std::array<std::string, 6> suspects;
+extern const std::array<std::string, 7> weapons;
+extern const std::array<std::string, 9> rooms;
+extern const std::array<std::string, 22> all_cards;
 
 }
-
 
